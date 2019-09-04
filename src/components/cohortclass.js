a8daec7fe0bb46bc8a1180a3fed3f9f9488f2c9b
Jaako Andes <jaako.andes@boom.camp>
2019-09-04 13:40:48 +0800

    Removed the search bar

--- src/components/cohortclass.js
+++ src/components/cohortclass.js
@@ -1,16 +1,6 @@
 import React from "react";
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import Button from '@material-ui/core/Button'
-import Header from "./header";
-import { graphql, navigate } from "gatsby";
-import CircularProgress from '@material-ui/core/CircularProgress';
-import {BoxMain, BoxCont, SearchBoxNav, MainContainer, ResultNav} from "../style/cohortDesign";
+import {BoxMain, BoxCont, MainContainer, ResultNav} from "../style/cohortDesign";
 
 
 export default class CohortClass extends React.Component {
@@ -22,40 +12,12 @@ export default class CohortClass extends React.Component {
         return (
             <React.Fragment>
                 <MainContainer>
-                    <SearchBoxNav>
-                        <Paper style={{
-                            padding: '2px 4px',
-                            display: 'flex',
-                            alignItems: 'center',
-                            width: '400px'
-                        }}>
-                            <IconButton style={{padding: '10px'}} aria-label="menu">
-                                <MenuIcon />
-                            </IconButton>
-                            <InputBase
-                                style={{marginLeft: 8, flex: 1}}
-                                placeholder="Search Cohort Class"
-                            />
-                            <IconButton style={{padding: '10px'}} aria-label="search">
-                                <SearchIcon />
-                            </IconButton>
-                            <Divider style={{height: 28, margin: 4}} orientation="vertical" />
-                        </Paper>
-                    </SearchBoxNav>
                     <ResultNav>
                         <Typography variant="caption">
                             About {this.props.classList.length} results.
                         </Typography>
                     </ResultNav>
                     <BoxMain>
-                        {/*
-                            If the result/data is empty
-                            <Typography variant="h4" style={{color: '#1f1e1c'}}>Empty!</Typography> 
-                        */}
-                        {/* 
-                            If the result/data is loading
-                            <CircularProgress />
-                        */}
                         {
                             this.props.classList.map(x => {
                                 return (
